b9e84e324342d867dfb36e55bbbc5e302f1d7343
dreamsdeng <2998677551@qq.com>
2018-11-07 22:10:50 +0800

    第二次提交所有内容

--- src/main/java/com/example/gitproject/SecondActivity.java
+++ src/main/java/com/example/gitproject/SecondActivity.java
@@ -1,15 +1,15 @@
 package com.example.gitproject;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
         Log.e("denganzhi1","hello.javac-.....");
     }
 }
